d60657a65bedf43c5f0362372a67af9676c0a0d2
User <User@GB-206-B3>
2026-01-10 16:38:36 +0800

    加入專案檔案。

---
+++ CourseWeb/Models/CourseScheduleViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace CourseWeb.Models
+{
+    public class CourseScheduleViewModel
+    {
+        public Guid Id { get; set; }
+        public string CourseCode { get; set; }
+        public string CourseName { get; set; }
+        public string TeacherName { get; set; }
+
+        public int Times { get; set; }
+
+        public DateOnly StartDate { get; set; }
+
+        public DateOnly EndDate { get; set; }
+
+        public string Localation {  get; set; }
+
+        public string Dec { get; set; }
+
+
+    }
+}

f2703eb8c1af6e7ccf0b55f073f976c7dae91bf7
DrinkAsWater <gtasa12500@gmail.com>
2026-01-17 20:47:27 +0800

    2026/01/17

---
+++ CourseService/Interface/IUserRespository.cs
@@ -0,0 +1,14 @@
+﻿using CourseService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseService.Interface
+{
+    public interface IUserRespository
+    {
+        Task<bool> CreateAsync(UserModel user);
+    }
+}

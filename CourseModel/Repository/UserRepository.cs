cc94351fd2419ccedb9d2fa7ed6a821297731c23
DrinkAsWater <gtasa12500@gmail.com>
2026-01-19 16:57:06 +0800

    2026/1/19

---
+++ CourseModel/Repository/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using CourseService.Interface;
+using CourseService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseData.Repository
+{
+ 
+    public class UserRepository : IUserRespository
+    {
+        private readonly IUserRespository _userRespository;
+
+        public UserRepository(IUserRespository userRespository)
+        {
+            _userRespository = userRespository;
+        }
+        public Task<bool> CreateAsync(UserModel user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

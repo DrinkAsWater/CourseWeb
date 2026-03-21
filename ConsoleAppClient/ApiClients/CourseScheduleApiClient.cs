b489ddb6dff8962b5bd3a465e74ba179ffe1489d
User <User@GB-206-B3>
2026-03-21 16:06:05 +0800

    [新增]console=>新增資料夾ApiAppClient

---
+++ ConsoleAppClient/ApiClients/CourseScheduleApiClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppClient.ApiClients
+{
+    public class CourseScheduleApiClient
+    {
+        private readonly HttpClient _httpclient;
+        public CourseScheduleApiClient(HttpClient client) {
+
+            _httpclient = client;
+        }
+    }
+}

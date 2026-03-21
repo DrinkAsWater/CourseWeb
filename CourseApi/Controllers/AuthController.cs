7094f8d7cc380b6db974e6be837dc1cee7959e05
User <User@GB-206-B3>
2026-03-21 09:51:45 +0800

    上課進度

---
+++ CourseApi/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using CourseApi.security;
+using CourseService.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace CourseApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly JwtHelper _jwtHelper;
+        public AuthController(IUserService userService, JwtHelper jwtHelper)
+        {
+            _userService = userService;
+            _jwtHelper = jwtHelper;
+        }
+
+    }
+}

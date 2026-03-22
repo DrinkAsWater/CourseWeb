29763e969becf3800054edc82ca771446d4c75f7
DrinkAsWater <gtasa12500@gmail.com>
2026-03-23 00:53:26 +0800

    Reapply "NotFisnish"
    This reverts commit c9cd2ddff8a4c279c1076a97dbac62269bce97c8.

---
+++ ConsoleAppClient/Models/CourseScheduleModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppClient.Models
+{
+    public class CourseScheduleModel
+    {
+        public Guid Id { get; set; }
+
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public string TeacherName { get; set; }
+
+        public string Desc { get; set; } = string.Empty;
+
+        public int Times { get; set; }
+
+        public DateOnly Sdate { get; set; }
+
+        public DateOnly Edate { get; set; }
+
+        public string Location { get; set; } = string.Empty;
+    }
+}

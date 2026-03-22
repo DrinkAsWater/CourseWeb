c9cd2ddff8a4c279c1076a97dbac62269bce97c8
DrinkAsWater <gtasa12500@gmail.com>
2026-03-23 00:52:54 +0800

    Revert "NotFisnish"
    This reverts commit 4e3cf9831a9e6d966626c96cfb8b0486bb32cb2a.

--- ConsoleAppClient/ApiClients/CourseScheduleApiClient.cs
+++ ConsoleAppClient/ApiClients/CourseScheduleApiClient.cs
@@ -1,8 +1,6 @@
-﻿using ConsoleAppClient.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,23 +12,6 @@ namespace ConsoleAppClient.ApiClients
         public CourseScheduleApiClient(HttpClient client) {
 
             _httpclient = client;
-        }
-        public async Task<List<CourseScheduleModel>?> GetCourseAsync()
-        {
-        Console.WriteLine($"呼叫API: GET {_httpclient.BaseAddress}api/CourseSchedule");
-            var response = await _httpclient.GetAsync("api/CourseSchedule");
-            if (response.IsSuccessStatusCode)
-            {
-            var course = await response.Content.ReadFromJsonAsync<List<CourseScheduleModel>>();
-                Console.WriteLine($"成功取得:{course?.Count ?? 0}筆課程資料");
-                return course;
-            }
-            else
-            {
-                var errorContet = await response.Content.ReadAsStringAsync();
-                Console.WriteLine($"")
-            }
-
         }
     }
 }
